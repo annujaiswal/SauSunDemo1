d9aae4c54604ba1ced17769f6cfe16561bbd1713
annujaiswal <annujaiswal31@gmail.com>
2021-05-30 21:51:13 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -8,3 +8,12 @@
         } 
     });  
   });
+ $(document).ready(function() {
+    $('#autoWidth1').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth1').removeClass('cS-hidden');
+        } 
+    });  
+  });
